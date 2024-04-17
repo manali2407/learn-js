237d1ae3ce9fd1af49898da2a7521bc600bd2d0c
manali2407 <manali2407@gmail.com>
2024-04-17 11:17:21 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,19 @@
+
+// datatype
+// number => 2 to power 53
+// BigInt
+// string => ""
+// Boolean => true/false
+// null =>standalone value
+// undefined =>
+// Symbol=> unique
+
+let age = 25
+let firstName="manali"
+let logIn = true
+let city = null
+let state = undefined
+
+console.table([typeof(age), typeof(firstName), typeof(logIn),typeof(city),typeof(state)]);
+
+console.table(typeof[(age),(firstName),(logIn),(city),(state)]);
