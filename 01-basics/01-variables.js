ed38cfa861a96f698601994e943af747c66fa0e4
manali2407 <manali2407@gmail.com>
2024-04-17 10:52:27 +0000

    variables

--- 01-basics/01-variables.js
+++ 01-basics/01-variables.js
@@ -1,1 +1,15 @@
-const 
+const accountId = 123456;
+let accountEmail = "manali@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState;
+let accountAddress = "";
+let accountAddress1 = null;
+// accountId = 2 // not allowed
+
+accountEmail = "mp@gmail.com"
+accountPassword="232323"
+accountCity="Thane"
+
+console.log(accountId);
+console.table([accountId, accountEmail , accountPassword, accountCity, accountState,accountAddress, accountAddress1]);
