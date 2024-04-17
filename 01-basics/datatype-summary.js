980c0a679c4006e734e4fd9a74489ac6553e94de
manali2407 <manali2407@gmail.com>
2024-04-17 15:03:06 +0000

    datatype-summary

---
+++ 01-basics/datatype-summary.js
@@ -0,0 +1,37 @@
+//primative data type
+
+// 7 type number, string, boolean, null, undefined, symbol,bigInt
+
+const score = 100
+const isLoggedIn = true
+const outsideTemp = null
+let userName;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+const bigIntNumber = 98585256852558
+// refernce type or non primative
+
+
+//object , array, functions
+  
+   dev = ["Brahma", "Vishnu", "Mahesh"];
+
+   let devi=[{
+      name:"Saraswati",
+      husband:"Brahma"
+   },
+   {
+    name:"Laxmi",
+    husband:"Vishnu"
+ },
+ {
+    name:"Parvati",
+    husband:"Mahesh"
+ },
+
+]
+
+
+const myFunction = function(){
+    console.log("hello world");
+}
