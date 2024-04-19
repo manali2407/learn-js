94a5377d3ef46f2320c0c7aca825aea05d12cbc5
manali2407 <manali2407@gmail.com>
2024-04-19 08:19:04 +0000

    date

---
+++ 01-basics/07-dates.js
@@ -0,0 +1,22 @@
+// dates
+
+let myDate = new Date()
+
+// console.log(myDate)
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toUTCString())
+
+// let myDate1 = new Date(2024,3,24);
+//  let myDate1 = new Date(2024,3,24,5,30);
+let myDate1 = new Date("2024-07-24");
+// console.log(myDate1.toString())
+
+
+// ??/////////////////timestapmes
+
+let mytimestamp = Date.now();
+console.log(mytimestamp);
+console.log(Math.round(Date.now()/1000));
