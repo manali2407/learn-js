ebba9f184483ee5fbdcf3c5e08d266289e3edd50
manali2407 <manali2407@gmail.com>
2024-04-19 09:58:43 +0000

    aaray1

---
+++ 02-basics/01_array.js
@@ -0,0 +1,33 @@
+
+//array
+
+const  arry = [0,1,2,3,4,5];
+
+let arry1 = ["Barmha", "Vishnu", "Mahesh","Saraswati", "Laxmi", "Parvati "];
+//console.log(arry[5]);
+
+// array methods
+
+// arry.push(6);
+// arry.push(7);
+// arry.pop()
+
+// arry.unshift(9);
+// arry.shift();
+// console.log(arry.includes(3));
+// console.log(arry.indexOf(5));
+
+const newArr = arry.join()
+
+// console.log(newArr);
+
+// console.log(arry);
+
+console.log("A", arry)
+
+let myn1 = arry.slice(1,5);
+console.log("B", myn1);
+
+let myn2 = arry.splice(1,3);
+console.log("C", myn2);
+console.log("A og", arry);
