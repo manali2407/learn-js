5a9eb019ddcd40ba87e6e573f4f5eb868e68f9e2
manali2407 <manali2407@gmail.com>
2024-04-19 14:50:01 +0000

    aaray2

---
+++ 02-basics/02_arrays.js
@@ -0,0 +1,27 @@
+
+let dev = ["Barmha", "Vishnu", "Mahesh"];
+let devi = ["Saraswati", "Laxmi", "Parvati "];
+
+// dev.push(devi);
+// console.log(dev);
+// console.log(dev[3][1]);
+
+// const allGod = dev.concat(devi);
+// console.log(allGod);
+
+// const all_God = [...dev,...devi];
+// console.log(all_God);
+
+
+// const another_array = [0,1,2,3,[4,5],6,7,[8,9,[5,8,[2,5]]]];
+
+// const flatArray = another_array.flat(Infinity);
+// console.log(flatArray);
+
+console.log(Array.isArray("Manali"));
+console.log(Array.from("Manali"));
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
