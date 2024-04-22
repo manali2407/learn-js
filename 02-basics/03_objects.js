22403eaacc38319de79ddac375202abdc2dc341a
manali2407 <manali2407@gmail.com>
2024-04-22 11:23:34 +0000

    objects

--- 02-basics/03_objects.js
+++ 02-basics/03_objects.js
@@ -9,5 +9,23 @@
     lastLoginDays:["Monday","Saturday"]
  }
 
- console.log(jsUser.email);
- console.log(jsUser["email"]);
+//  console.log(jsUser.email);
+//  console.log(jsUser["email"]);
+
+//  jsUser.email = "manali@mail2.com"
+//  console.log(jsUser.email);
+//  Object.freeze(jsUser);
+//  jsUser.email = "manali@mail1.com"
+//  console.log(jsUser.email);
+
+jsUser.greeting = function(){
+   console.log("hello world")
+}
+
+jsUser.greeting2 = function(){
+   console.log(`Hello ${this.name} well come to js tutorial` )
+}
+
+
+console.log(jsUser.greeting());
+console.log(jsUser.greeting2());
