6ad476f5ff027ed8eddb0596dfb80def7025c07e
manali2407 <manali2407@gmail.com>
2024-04-22 11:41:56 +0000

    objects

---
+++ 02-basics/04_objects.js
@@ -0,0 +1,32 @@
+//const user = new Object();
+const user = {}
+user.id=125;
+user.name = "Manali"
+user.IsLogged = true
+//console.log(user)
+
+const regularUser = {
+    email:"abc@gmail.com",
+    fullname:{
+        userFullName:{
+            firstname:"abc"
+        }
+    }
+}
+//console.log(regularUser.fullname.userFullName.fullname);
+
+obj1 = {1:"a",2:"c",3:"c"}
+obj2 = {4:"d",5:"e",6:"f"}
+
+
+//obj3 = {obj1,obj2}
+// const obj3 = Object.assign(obj1,obj2);
+// const obj3 = Object.assign({},obj1,obj2);
+
+const obj3 = {...obj1, ...obj2}  // mostly used opertator
+console.log(obj3);
+
+
+const stringVal = "manali pathratkar"
+const newVal = [...stringVal]
+console.log(newVal);
