bc056435f4902080bb9dca9c9e78bea1823e6067
manali2407 <manali2407@gmail.com>
2024-04-22 11:12:05 +0000

    objects

---
+++ 02-basics/03_objects.js
@@ -0,0 +1,13 @@
+// object literals
+
+ const jsUser = {
+    name:"Manali",
+    age:30,
+    location:"Mumbai",
+    email:"manali@mail.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"]
+ }
+
+ console.log(jsUser.email);
+ console.log(jsUser["email"]);
