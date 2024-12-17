f5270bfdc7ac5b0eeb82936994f573f710f9cb5c
manali2407 <manali2407@gmail.com>
2024-12-17 16:10:47 +0000

    change

--- 02-basics/01_array.js
+++ 02-basics/01_array.js
@@ -1,12 +1,12 @@
 
-//array
+// //array
 
 const  arry = [0,1,2,3,4,5];
 
 let arry1 = ["Barmha", "Vishnu", "Mahesh","Saraswati", "Laxmi", "Parvati "];
 //console.log(arry[5]);
 
-// array methods
+// // array methods
 
 // arry.push(6);
 // arry.push(7);
@@ -31,3 +31,34 @@ console.log("B", myn1);
 let myn2 = arry.splice(1,3);
 console.log("C", myn2);
 console.log("A og", arry);
+
+
+// const myArray = [0,1,2,3,4,5]
+
+// const myPush = myArray.push(6)
+// //console.log("myPush", myPush, myArray);
+
+
+// const myPop = myArray.pop()
+// //console.log("myPop", myPop,myArray);
+
+// const myUnshift = myArray.unshift(22)
+// //console.log("myUnshift", myArray);
+
+// const myshift =  myArray.shift()
+// //console.log("myshift", myArray);
+
+
+// // slice an splice
+
+// const myslice = myArray.slice(1,3)
+// console.log("myslice", myslice);
+// console.log("og myArray", myArray);
+
+
+// const mysplice = myArray.splice(1,3)
+// console.log("mysplice", mysplice);
+// console.log("og myArray", myArray);
+
+
+
