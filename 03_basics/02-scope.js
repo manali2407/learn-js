a6b359dfaa450588c86e122012563092f6211300
manali2407 <manali2407@gmail.com>
2024-12-19 06:46:39 +0000

    scope

---
+++ 03_basics/02-scope.js
@@ -0,0 +1,15 @@
+//var c = 300  
+
+let a = 200;
+
+if (true) {
+    let a = 10;
+    console.log("Inner a", a);
+    const b = 20;
+    //var c = 30  
+}
+
+
+console.log(a);
+//console.log(b); // not allow
+//console.log(c); // not allow
